ee969885109dab70515d4c246f824e2b347b5194
taed13 <taedtech13@gmail.com>
2024-01-17 22:52:53 +0700

    fixed header, change tailwind config

--- public/src/pages/HomePage.jsx
+++ public/src/pages/HomePage.jsx
@@ -11,76 +11,75 @@ import fieldOperation3 from "../assets/TRAVELEVENTS.jpg";
 const contentStyle = {
   color: "#fff",
   textAlign: "center",
-  background: "#364d79",
 };
 
 function HomePage() {
   return (
     <>
-      <div className="flex flex-col mx-auto">
-        <Carousel autoplay>
-          <div>
-            <img
-              src={banner1}
-              alt=""
-              style={contentStyle}
-              className="w-full h-96 object-cover"
-            />
-            {/* <h3 style={contentStyle}>1</h3> */}
-          </div>
-          <div>
-            <img
-              src={banner2}
-              alt=""
-              style={contentStyle}
-              className="w-full h-96 object-cover"
-            />
-          </div>
-          <div>
-            <img
-              src={banner3}
-              alt=""
-              style={contentStyle}
-              className="w-full h-96 object-cover"
-            />
-          </div>
-        </Carousel>
-      </div>
-
-      <div className="text-center md:container md:mx-auto mt-5">
-        <p className="text-sky-400 uppercase font-bold text-xl py-4">
-          Lĩnh vực hoạt động
-        </p>
-        <div className="flex flex-wrap justify-center gap-7 mt-3 mb-5">
-          <div className="w-1/4 relative">
-            <img
-              src={fieldOperation1}
-              alt=""
-              className="w-full rounded-lg transform hover:scale-105 transition-transform"
-            />
-            <p className="text-xl text-white font-bold py-2 absolute bottom-0 left-0 bg-opacity-50 w-full text-center">
-              Bất động sản
-            </p>
-          </div>
-          <div className="w-1/4 relative">
-            <img
-              src={fieldOperation2}
-              alt=""
-              className="w-full rounded-lg transform hover:scale-105 transition-transform"
-            />
-            <p className="text-xl text-white font-bold py-2 absolute bottom-0 left-0 bg-opacity-50 w-full text-center">
-              Tư vấn tài chính
-            </p>
-          </div>
-          <div className="w-1/4 relative">
-            <img
-              src={fieldOperation3}
-              alt=""
-              className="w-full rounded-lg transform hover:scale-105 transition-transform"
-            />
-            <p className="text-xl text-white font-bold py-2 absolute bottom-0 left-0 bg-opacity-50 w-full text-center">
-              Lữ hành & Sự kiện
-            </p>
+      <div>
+        <div className="flex flex-col mx-auto">
+          <Carousel autoplay>
+            <div>
+              <img
+                src={banner1}
+                alt=""
+                style={contentStyle}
+                className="w-full h-128 object-cover"
+              />
+            </div>
+            <div>
+              <img
+                src={banner2}
+                alt=""
+                style={contentStyle}
+                className="w-full h-128 object-cover"
+              />
+            </div>
+            <div>
+              <img
+                src={banner3}
+                alt=""
+                style={contentStyle}
+                className="w-full h-128 object-cover"
+              />
+            </div>
+          </Carousel>
+        </div>
+        <div className="text-center md:container md:mx-auto mt-5">
+          <p className="text-sky-400 uppercase font-bold text-xl py-4">
+            Lĩnh vực hoạt động
+          </p>
+          <div className="flex flex-wrap justify-center gap-7 mt-3 mb-5">
+            <div className="w-1/4 relative">
+              <img
+                src={fieldOperation1}
+                alt=""
+                className="w-full rounded-lg transform hover:scale-105 transition-transform"
+              />
+              <p className="text-xl uppercase text-white font-bold py-2 absolute bottom-0 left-0 bg-opacity-50 w-full text-center">
+                Bất động sản
+              </p>
+            </div>
+            <div className="w-1/4 relative">
+              <img
+                src={fieldOperation2}
+                alt=""
+                className="w-full rounded-lg transform hover:scale-105 transition-transform"
+              />
+              <p className="text-xl uppercase text-white font-bold py-2 absolute bottom-0 left-0 bg-opacity-50 w-full text-center">
+                Tư vấn tài chính
+              </p>
+            </div>
+            <div className="w-1/4 relative">
+              <img
+                src={fieldOperation3}
+                alt=""
+                className="w-full rounded-lg transform hover:scale-105 transition-transform"
+              />
+              <p className="text-xl uppercase text-white font-bold py-2 absolute bottom-0 left-0 bg-opacity-50 w-full text-center">
+                Lữ hành & Sự kiện
+              </p>
+            </div>
           </div>
         </div>
       </div>
